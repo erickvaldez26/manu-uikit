6d9aa9c273e2e739dd6ac93d568de09df68749a8
erickvaldez26 <apata26erick@hotmail.com>
2025-04-25 22:35:19 -0500

    initial commit

---
+++ manu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  manu
+//
+//  Created by Erick Valdez on 25/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
